d6ade70a2bda1c329b4e80c6075884947e8653b4
Khurshidbek Kobilov <kabilovhurshid5@gmail.com>
2022-01-21 03:20:19 +0500

    get posts finished

---
+++ resources/js/components/Posts/Index.vue
@@ -0,0 +1,35 @@
+<template>
+    <table class="table">
+        <thead>
+        <tr>
+            <th>Title</th>
+            <th>Post Text</th>
+            <th>Created Date</th>
+            <th>Action</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="post in posts">
+            <td>{{ post.title }}</td>
+            <td>{{ post.text }}</td>
+            <td>{{ post.created_at }}</td>
+            <td></td>
+        </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                posts: []
+            }
+        },
+        mounted() {
+            axios.get('/api/posts').then(res => {
+                this.posts = res.data.data;
+            })
+        }
+    }
+</script>
